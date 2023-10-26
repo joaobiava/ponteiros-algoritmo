d5daca87bf2cfeba603b4f662ae44bd23fac9499
joaobiava <138793503+joaobiava@users.noreply.github.com>
2023-10-26 19:04:34 -0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int tam;
+    int *p;
+    cin >> tam;
+    int v[tam];
+    for(int i=0; i<tam; i++){
+        cin >> v[i];
+    }
+    for(int i=0; i<tam; i++){
+        *p = v[i];
+        cout << *p << " ";
+    }
+}
---
+++ 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void inverter(string *palavra){
+    string invertida;
+    for(int i=(*palavra).length(); i>=0; i--){
+        invertida += (*palavra)[i];
+    }
+    *palavra = invertida;
+}
+
+int main(){
+    string palavra;
+    int p;
+    cin >> palavra;
+    inverter(&palavra);
+    cout << palavra;
+}
